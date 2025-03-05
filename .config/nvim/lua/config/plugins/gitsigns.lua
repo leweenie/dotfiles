ae3b6def20765441b35dd1bf82e4120af142f4ec
leweenie <tleween13@gmail.com>
2025-03-05 03:02:07 -0500

    added gitsigns to neovim conf

---
+++ .config/nvim/lua/config/plugins/gitsigns.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsigns = require("gitsigns")
+
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "+" },
+          change = { text = "~" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
+        },
+      })
+
+      -- navigation
+      vim.keymap.set("n", "]c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gitsigns.nav_hunk("next")
+        end
+      end)
+
+      vim.keymap.set("n", "[c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gitsigns.nav_hunk("prev")
+        end
+      end)
+
+      -- preview changes
+      vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk)
+    end,
+  },
+}
