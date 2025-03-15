3b40aa79d6e8f2566410546159decbca7beb51ca
leweenie <tleween13@gmail.com>
2025-03-15 13:42:14 -0400

    updated toggleterm.nvim settings

--- .config/nvim/lua/config/plugins/terminal.lua
+++ .config/nvim/lua/config/plugins/terminal.lua
@@ -3,8 +3,11 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
+      vim.keymap.set("n", "<C-\\>", "<cmd>ToggleTerm<cr>")
       require("toggleterm").setup({
         direction = "float",
+        size = 10,
+        open_mapping = [[<c-\>]],
       })
     end,
   },
