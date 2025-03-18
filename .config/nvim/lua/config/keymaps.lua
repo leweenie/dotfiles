d25e327b72d5fc1049e0dc816394c9b031210b91
leweenie <tleween13@gmail.com>
2025-03-18 13:18:26 -0400

    gen updates

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,13 @@ local opts = { noremap = true, silent = true }
 
 -- oil.nvim toggle
 vim.api.nvim_create_user_command("OilToggle", function()
-	vim.cmd((vim.bo.filetype == "oil") and "bd" or "Oil")
+  vim.cmd((vim.bo.filetype == "oil") and "bd" or "Oil")
 end, { nargs = 0 })
 vim.keymap.set("n", "<Bslash>", "<cmd>OilToggle<cr>", opts)
 
 -- text management
-vim.keymap.set("n", "<A-j>", ":m .+1<cr>==", opts) -- move line up(n)
-vim.keymap.set("n", "<A-k>", ":m .-2<cr>==", opts) -- move line down(n)
+vim.keymap.set("n", "<A-j>", ":m .+1<cr>==", opts)     -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<cr>==", opts)     -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", opts) -- move line up(v)
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", opts) -- move line down(v)
 
@@ -23,8 +23,8 @@ vim.keymap.set("n", "<S-h>", "<cmd>bp<cr>", opts) -- move buffer prev
 -- window resize
 vim.keymap.set("n", "<A-S-k>", "<cmd>horizontal resize +5<cr>", opts) -- horizontal (taller)
 vim.keymap.set("n", "<A-S-j>", "<cmd>horizontal resize -5<cr>", opts) -- horizontal (shorter)
-vim.keymap.set("n", "<A-S-l>", "<cmd>vertical resize +10<cr>", opts) -- vertical (wider)
-vim.keymap.set("n", "<A-S-h>", "<cmd>vertical resize -10<cr>", opts) -- vertical (thinner)
+vim.keymap.set("n", "<A-S-l>", "<cmd>vertical resize +10<cr>", opts)  -- vertical (wider)
+vim.keymap.set("n", "<A-S-h>", "<cmd>vertical resize -10<cr>", opts)  -- vertical (thinner)
 
 -- exit terminal mode
 vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
@@ -47,29 +47,29 @@ vim.keymap.set("n", "<leader>cl", "<cmd>bd!<cr>", opts)
 
 -- cycle lsp diagnostics
 vim.keymap.set("n", "<C-n>", function()
-	vim.diagnostic.goto_next()
+  vim.diagnostic.goto_next()
 end)
 vim.keymap.set("n", "<C-p>", function()
-	vim.diagnostic.goto_prev()
+  vim.diagnostic.goto_prev()
 end)
 
 -- gitsigns keymaps
 local gitsigns = require("gitsigns")
 
 vim.keymap.set("n", "<leader>n", function() -- goto next hunk
-	if vim.wo.diff then
-		vim.cmd.normal({ "<leader>n", bang = true })
-	else
-		gitsigns.nav_hunk("next")
-	end
+  if vim.wo.diff then
+    vim.cmd.normal({ "<leader>n", bang = true })
+  else
+    gitsigns.nav_hunk("next")
+  end
 end)
 
 vim.keymap.set("n", "<leader>p", function() -- goto previous hunk
-	if vim.wo.diff then
-		vim.cmd.normal({ "<leader>p", bang = true })
-	else
-		gitsigns.nav_hunk("prev")
-	end
+  if vim.wo.diff then
+    vim.cmd.normal({ "<leader>p", bang = true })
+  else
+    gitsigns.nav_hunk("prev")
+  end
 end)
 
 vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk) -- open preivew
--- .config/nvim/lua/config/plugins/terminal.lua
+++ .config/nvim/lua/config/plugins/terminal.lua
@@ -3,12 +3,24 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-      vim.keymap.set("n", "<C-\\>", "<cmd>ToggleTerm<cr>")
       require("toggleterm").setup({
         direction = "float",
         size = 10,
-        open_mapping = [[<c-\>]],
+        -- open_mapping = [[<c-\>]],
       })
+
+      vim.keymap.set({ "i", "n", "t" }, "<C-Bslash>", function()
+        local file_type = vim.bo.filetype
+        local file_path = vim.fn.expand("%:.")
+
+        if file_type == "python" then
+          vim.cmd("TermExec cmd='python " .. file_path .. "'")
+        elseif file_type == "go" then
+          vim.cmd("TermExec cmd='go run " .. file_path .. "'")
+        else
+          vim.cmd("ToggleTerm")
+        end
+      end)
     end,
   },
 }
