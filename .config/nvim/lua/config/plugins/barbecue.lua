5c28b02132d520e8209b0225e9bef1735b29c9d1
leweenie <tleween13@gmail.com>
2025-04-18 17:45:18 -0400

    general updates

--- .config/nvim/lua/config/plugins/barbecue.lua
+++ .config/nvim/lua/config/plugins/barbecue.lua
@@ -13,6 +13,7 @@ return {
       require("barbecue").setup({
         theme = {
           normal = { bg = "#000000" },
+          -- normal = { bg = "#1c1c1c" },
           -- normal = { bg = "#090909" },
           -- normal = { bg = "#262626" },
         },
